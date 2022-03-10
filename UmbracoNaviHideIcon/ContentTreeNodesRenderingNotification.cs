66f301d2eb447fbe8e48e9fa51183a784e0f04ee
Johannes Lantz <lithvia@gmail.com>
2022-03-10 16:10:24 +0100

    Git init

---
+++ UmbracoNaviHideIcon/ContentTreeNodesRenderingNotification.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Extensions;
+
+namespace UmbracoNaviHideIcon
+{
+	public class ContentTreeNodesRenderingNotification : INotificationHandler<TreeNodesRenderingNotification>
+	{
+		private readonly IUmbracoContextFactory _umbracoContextFactory;
+		public ContentTreeNodesRenderingNotification(IUmbracoContextFactory umbracoContextFactory)
+		{
+			_umbracoContextFactory = umbracoContextFactory;
+		}
+		public void Handle(TreeNodesRenderingNotification notification)
+		{
+			if (notification.TreeAlias.Equals("content"))
+			{
+				foreach (var node in notification.Nodes)
+				{
+					using (UmbracoContextReference umbracoContextReference = _umbracoContextFactory.EnsureUmbracoContext())
+					{
+						var umbracoNode = umbracoContextReference.UmbracoContext.Content.GetById(int.Parse(node.Id.ToString()));
+						if (umbracoNode != null && umbracoNode.HasValue("umbracoNaviHide") && umbracoNode.Value<bool>("umbracoNaviHide"))
+						{
+							node.CssClasses.Add("umbracoNaviHideIcon");
+						}
+					}
+					
+				}
+			}
+		}
+	}
+}
---
+++ UmbracoNaviHideIcon/NotificationComposer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Notifications;
+
+namespace UmbracoNaviHideIcon
+{
+	public class NotificationComposer : IUserComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			builder.AddNotificationHandler<TreeNodesRenderingNotification, ContentTreeNodesRenderingNotification>();
+		}
+	}
+}
